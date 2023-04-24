23bc9f3737a7fc43ea1b5c0e77d100c00f9954b7
Carlos Paparoni <carlos.paparoni.fsl@crowdbotics.com>
2023-04-23 22:56:23 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "pettopia-cpaparoni--40153",
-  url: "https://pettopia-cpaparoni--40153.botics.co",
-  api: "https://pettopia-cpaparoni--40153.botics.co/api/v1"
-}
+  "name": "pettopia-cpaparoni--40153",
+  "url": "https://pettopia-cpaparoni--40153.botics.co",
+  "api": "https://pettopia-cpaparoni--40153.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
