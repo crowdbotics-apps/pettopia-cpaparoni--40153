6f692c91cc306177e64cb8b51ee9176de47d5757
Crowdbotics <team@crowdbotics.com>
2023-04-24 02:31:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pettopia-cpaparoni--40153",
+  url: "https://pettopia-cpaparoni--40153.botics.co",
+  api: "https://pettopia-cpaparoni--40153.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
